4f5b178f6688fc0d636ec6c93ec1ef41c2898f32
Andrea <90292543+amgallo1213@users.noreply.github.com>
2025-01-06 16:18:42 -0500

    added json file and script.js file

---
+++ script.js
@@ -0,0 +1,29 @@
+displayPerfumes()
+
+async function displayPerfumes() {
+    const perfumes = await getPerfumes()
+    console.log(perfumes)
+
+    const cardContainer = document.querySelector("#product_container")
+
+    const perfumeCards = () => {
+        perfumes.map((perfume) => {
+            cardContainer.innerHTML += `
+        <div class="card">
+          <img src="${perfume.img}" class="img">
+          <h2 class="card_name">${perfume.name}</h2>
+          <p class="keyWords">${perfume.keyWords}</p>
+        </div>
+      `
+        })
+    }
+    perfumeCards()
+
+}
+
+async function getPerfumes() {
+    const res = await fetch("perfume.json")
+    const data = await res.json()
+    return data
+}
+
